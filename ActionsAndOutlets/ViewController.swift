1451d5e6530639539542df10397b1643004368b3
Eric Smith <sneric.smith@gmail.com>
2020-04-19 12:35:31 -0500

    Iteration 0

--- ActionsAndOutlets/ViewController.swift
+++ ActionsAndOutlets/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var appTitle: UILabel!
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func submitButtonTapped(_ sender: UIButton) {
+        if let newTitle = textField.text {
+            appTitle.text = newTitle
+        }
+    
+    print("TEST")
+    }
+    
 }
 
